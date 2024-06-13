c079ba286d1f420cc8cc1d5457ff4ce82a56ba02
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2024-06-13 08:24:54 -0400

    Added pertinent python programs

---
+++ turn.py
@@ -0,0 +1,39 @@
+import turning_function
+import statistics as stat
+from polygon import Polygon
+import matplotlib.pyplot as plt
+
+p = Polygon()
+shape_a = p.regpoly(10)
+shape_b = p.randpoly(10)
+
+def turning_distance(shape_a, shape_b):
+	distance, _, _, _ = turning_function.distance(shape_a, shape_b, brute_force_updates=False)
+	return distance
+
+def multi_turning(regpoly, normpoly, iterations=1):
+	return [turning_distance(regpoly, normpoly) for _ in range(iterations)]
+
+if __name__ == "__main__":
+	p = Polygon()
+	sides = []
+	turn_dist = []
+	prevpoly = p.regpoly(2)
+	for i in range(3, 10):
+# 		print(i)
+		sides.append(i)
+		newpoly = p.regpoly(i)
+		turn_dist.append(stat.fmean(multi_turning(prevpoly, newpoly, iterations=10)))
+		prevpoly = newpoly
+
+	fig = plt.figure()
+	ax = fig.add_subplot()
+	ax.set_yscale('log')
+
+	plt.plot(sides, turn_dist)
+	
+	plt.title("N versus Turning Distance T(R_{n-1}, R_{n})")
+	plt.xlabel("Number of Sides")
+	plt.ylabel("Turning Distance")
+
+	plt.show()
---
+++ voronoi.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pyvoro
+import turning_function
+import matplotlib.pyplot as plt
+import statistics as stat
+
+from polygon import Polygon
+
+class Voronoi_Utils:
+    def __init__(self):
+        self.p = Polygon()
+    
+    def turn_dists(self, cells):
+        turn_distances = []
+        for i, cell in enumerate(cells):    
+            polygon = cell['vertices']
+            dist, _, _, _ = turning_function.distance(polygon, self.p.regpoly(len(polygon)), brute_force_updates=False)
+            turn_distances.append(dist)
+        return turn_distances
+    
+    def plot_diagram(self, points):
+        plt.plot(points[:,0], points[:,1], 'ko')
+        plt.xlim(-0.1, 1.1)
+        plt.ylim(-0.1, 1.1)
+
+        plt.show()
+    
+    def plot_turn_dists(self, x_vals, y_vals, N):
+        plt.scatter(x_vals, y_vals)
+        plt.xlabel("Number of cells")
+        plt.ylabel("Mean average turning distance")
+        plt.title("Average turning distance on Voronoi diagrams")
+        yerr = np.std(y_vals) / np.sqrt(30)
+        plt.errorbar(x_vals, y_vals, yerr=yerr, fmt='o')
+        plt.xticks(np.arange(0, N+1, step=100))
+        plt.show()
+
+if __name__ == "__main__":
+    v = Voronoi_Utils()
+    x_vals = []
+    means = []
+    N = 100
+    step = 50
+    rounds = range(1, 31)
+    np.random.seed([1938430])
+    for n in range(50, N+1, step):
+        print(n)
+        dists = []
+        for round in rounds:
+            print(f"dots_num = {n} --> round = {round}") 
+            points = np.random.rand(n, 2)
+            cells = pyvoro.compute_2d_voronoi(
+            points, # point positions, 2D vectors this time.
+            [[0.0, 1.0], [0.0, 1.0]], # box size
+            2.0, # block size
+            periodic = [True, True]
+            )
+            tds = v.turn_dists(cells)
+            dists.append(stat.fmean(tds))
+        x_vals.extend([n])
+        means.append(stat.fmean(dists))
+    
+    v.plot_turn_dists(x_vals, means, N)
