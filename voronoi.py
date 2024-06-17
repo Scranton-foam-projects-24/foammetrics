edba67337a7993f2b55122deb7062fb831a158b0
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2024-06-17 07:49:21 -0400

    updating main

--- voronoi.py
+++ voronoi.py
@@ -13,11 +13,15 @@ class Voronoi_Utils:
         turn_distances = []
         for i, cell in enumerate(cells):    
             polygon = cell['vertices']
-            dist, _, _, _ = turning_function.distance(polygon, self.p.regpoly(len(polygon)), brute_force_updates=False)
+            dist, _, _, _ = turning_function.distance(
+                polygon, 
+                self.p.regpoly(len(polygon)), 
+                brute_force_updates=False
+            )
             turn_distances.append(dist)
         return turn_distances
     
-    def plot_diagram(self, points):
+    def plot_diagram(self, points):        
         plt.plot(points[:,0], points[:,1], 'ko')
         plt.xlim(-0.1, 1.1)
         plt.ylim(-0.1, 1.1)
@@ -50,25 +54,51 @@ def avg_turn_dists(dots_num, step=50, rounds=30, init_num=50):
             # print(f"dots_num = {n} --> round = {round}") 
             points = np.random.rand(n, 2)
             cells = pyvoro.compute_2d_voronoi(
-            points, # point positions, 2D vectors this time.
-            [[0.0, 1.0], [0.0, 1.0]], # box size
-            2.0, # block size
-            periodic = [True, True]
+                points, # point positions, 2D vectors this time.
+                [[0.0, 1.0], [0.0, 1.0]], # box size
+                2.0, # block size
+                periodic = [True, True]
             )
             tds = v.turn_dists(cells)
             dists.append(np.mean(tds))
         deviations.append(np.std(dists) / np.sqrt(rounds[-1]))
         x_vals.extend([n])
         means.append(np.mean(dists))
-    
-    # print(deviations)
-    # print(f"graphing: {dots_num}")
-    # v.plot_turn_dists(x_vals, means, N, deviations)
+        
     return (x_vals, means, N, deviations)
-
-def foo(bar, result, index):
-    print( 'hello {0} from {1}'.format(bar, index))
-    result[index] = "foo"
+    # v.plot_turn_dists(x_vals, means, N, deviations)
 
 if __name__ == "__main__":
-    print("Don't run this file!")
+    v = Voronoi_Utils()
+    N = 20
+    points = np.random.rand(N, 2)
+    colors = np.random.rand(N, 3) 
+    color_map = {tuple(coords):color for coords, color in zip(points, colors)}
+    cells = pyvoro.compute_2d_voronoi(
+        points, # point positions, 2D vectors this time.
+        [[0.0, 1.0], [0.0, 1.0]], # box size
+        2.0, # block size
+        periodic = [True, True]
+    )
+    
+    
+    
+    for i, cell in enumerate(cells):    
+        polygon = cell['vertices']
+        plt.fill(*zip(*polygon),  color = 'black', alpha=0.1)
+    
+    v.plot_diagram(points)
+    
+    vertices = []
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    
+    for i, cell in enumerate(cells[0:5]):    
+        polygon = cell['vertices']
+        vertices.extend(polygon)
+        for i, edge in enumerate(np.array(polygon)):
+            ax.annotate(i, edge, xytext=[edge[0]+0.01, edge[1]+0.01])
+        plt.fill(*zip(*polygon),  color = 'black', alpha=0.1)
+        
+    v.plot_diagram(np.array(vertices))
+
