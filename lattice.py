cff80593da2493f2afc878890c28460c47aac2e7
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2024-06-26 08:59:18 -0400

    Encapsulated new code into single function

--- lattice.py
+++ lattice.py
@@ -23,68 +23,7 @@ def even_cell(G, m, n, pos, shapes, N):
     pos[five] = (1.0+xscale, (2*n)+(yscale))
     G.add_edges_from(edge_list)
     
-    # Bottom Triangle
-    shapes.append({'faces':
-      [
-        {'adjacent_cell': (n*3)-3 + (N*3)-1, 'vertices': [one, zero]},
-        {'adjacent_cell': (n*3)-2, 'vertices': [zero, two]},
-        {'adjacent_cell': (n*3)-4, 'vertices': [two, one]},
-       
-      ],
-      'original': np.array([
-          (pos[zero][0]+pos[one][0]+pos[two][0])/3,
-          (pos[zero][1]+pos[one][1]+pos[two][1])/3
-          ]),
-      'vertices':
-      [
-        np.array([pos[one][0], pos[one][1]]),
-        np.array([pos[zero][0], pos[zero][1]]),
-        np.array([pos[two][0], pos[two][1]])
-      ],
-      'volume': np.sqrt(3)/4,
-      'adjacency':[[one, zero], [zero, two], [two, one]]
-      })
-        
-    # Square
-    shapes.append({'faces':
-      [
-        {'adjacent_cell': (n*3)-2 - (N*3), 'vertices': [zero, five]},
-        {'adjacent_cell': (n*3)-1, 'vertices': [five, three]},
-        {'adjacent_cell': (n*3)-2 + (N*3), 'vertices': [three, two]},
-        {'adjacent_cell': (n*3)-3, 'vertices': [two, zero]},
-      ],
-      'original': np.array([(pos[zero][0]+pos[two][0])/2, (pos[zero][1]+pos[five][1])/2]),
-      'vertices':
-      [
-        np.array([pos[zero][0], pos[zero][1]]),
-        np.array([pos[five][0], pos[five][1]]),
-        np.array([pos[three][0], pos[three][1]]),
-        np.array([pos[two][0], pos[two][1]])
-      ],
-      'volume': 1,
-      'adjacency':[[zero, five], [five, three], [three, two], [two, zero]]
-      })
-        
-    # Top Triangle
-    shapes.append({'faces':
-      [
-        {'adjacent_cell': (n*3)-1 - (N*3)-1, 'vertices': [four, three]},
-        {'adjacent_cell': (n*3)-2, 'vertices': [three, five]},
-        {'adjacent_cell': (n*3), 'vertices': [five, four]},
-      ],
-      'original': np.array([
-          (pos[three][0]+pos[four][0]+pos[five][0])/3,
-          (pos[three][1]+pos[four][1]+pos[five][1])/3
-          ]),
-      'vertices':
-      [
-        np.array([pos[four][0], pos[four][1]]),
-        np.array([pos[three][0], pos[three][1]]),
-        np.array([pos[five][0], pos[five][1]])
-      ],
-      'volume': np.sqrt(3)/4,
-      'adjacency':[[four, five], [five, three], [three, four]]
-      })
+    add_shapes(pos, shapes, n-1)
 
 def odd_cell(G, m, n, pos, shapes, N):
     n += 1
@@ -106,6 +45,17 @@ def odd_cell(G, m, n, pos, shapes, N):
     pos[four] = (1.0+xscale, ((2*n)+1)+(yscale))
     pos[five] = (0.5+xscale, (2*n)+(yscale))
     G.add_edges_from(edge_list)
+    
+    add_shapes(pos, shapes, n-1)
+
+def add_shapes(pos, shapes, n):
+    n += 1
+    zero = (4*n)-4+(2*m)
+    one = (4*n)-3+(2*m)
+    two = (4*n)-2+(2*m)
+    three = (4*n)-1+(2*m)
+    four = 4*n+(2*m)
+    five = (4*n)+1+(2*m)
     
     # Bottom Triangle
     shapes.append({'faces':
